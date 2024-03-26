1ea6ea8511ee2aa29a27157e64e042b30280377a
Enoxden <163399021+Enoxden@users.noreply.github.com>
2024-03-26 16:54:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254742378511', 'ENOX', true],
+  ['254742378511', 'Aedan', true], 
   [''] 
 ] //Number of owners
 
